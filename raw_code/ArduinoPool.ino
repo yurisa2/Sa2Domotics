77075087d4f97041e3d42ec00fc979938654b424
yurisa2 <yuri@sa2.com.br>
2019-01-01 18:08:06 -0200

    Update ArduinoPool.ino

--- raw_code/ArduinoPool.ino
+++ raw_code/ArduinoPool.ino
@@ -2,13 +2,24 @@
 // If you're using another shield, see Boards_* examples
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <ESP8266WiFi.h>
+
+const char* ssid     = "Canaa";      // SSID
+const char* password = "nivealucasivan";      // Password
+const char* host = "sa2.com.br";  // IP serveur - Server IP
+const int   port = 80;            // Port serveur - Server Port
+const int   watchdog = 5000;        // Fréquence du watchdog - Watchdog frequency
+unsigned long previousMillis = millis(); 
+
+
 
 // Fio referente aos dados vai conectado ao pino 2
-#define ONE_WIRE_BUS 2
+#define ONE_WIRE_BUS 13
 
-#define HEAT_PUMP_PIN 2
-#define FULL_SYSTEM_RUNDOWN 180
-#define DELTA_HIST 0.5
+#define HEAT_PUMP_PIN 5
+#define MAIN_PUMP_PIN 4
+#define FULL_SYSTEM_RUNDOWN 60
+#define DELTA_HIST 1
 
 
 // Setup Inicial
@@ -23,28 +34,43 @@ long last_hour = 0;
 void setup(void)
 {
   // start serial port
-  Serial.begin(9600);
+  Serial.begin(115200);
+
+  // https://diyprojects.io/esp8266-web-client-tcp-ip-communication-examples-esp8266wifi-esp866httpclient/#.XCun91xKjIU
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  
+  
   // Start up the library
   sensors.begin();
 
   pinMode(HEAT_PUMP_PIN,OUTPUT);
+  pinMode(MAIN_PUMP_PIN,OUTPUT);
+
+  printTemps();
+  sendTemps();
+  runFullDiag();
 }
 
 void loop(void)
 { 
-
-  Serial.print("Esperando a temperatura...");
-  sensors.requestTemperatures();
-  Serial.println("Pronto");
-  Serial.print("Temperatura para o dispositivo 1 (index 0) é: ");
-  Serial.println(sensors.getTempCByIndex(0));  
-  Serial.print("Temperatura para o dispositivo 2 (index 1) é: ");
-  Serial.println(sensors.getTempCByIndex(1));  
-
   onLoop();
 }
 
 void onMinute() {
+  Serial.println("onMinute() ");
+  
+  printTemps();
+  sendTemps();
+  runHeatBreaker();
 
   last_minute = millis();
 }
@@ -55,6 +81,7 @@ void onSecond() {
 }
 
 void onHour() {
+  Serial.println("onHour() ");
 
   runDiagChooser();
 
@@ -64,8 +91,8 @@ void onHour() {
 void onLoop() {
    long mils = millis();
 
-   if(mils > (last_hour + 3600000)) onHour();
-   if(mils > (last_minute + 600000)) onMinute();
+   if(mils > (last_hour + 360000)) onHour();
+   if(mils > (last_minute + 60000)) onMinute();
    if(mils > (last_second + 1000)) onSecond();
 }
 
@@ -75,10 +102,29 @@ bool tempDeltaDiag() {
   else return false;
 }
 
+
+void printTemps() {
+
+  Serial.print("Getting Temps...");
+  sensors.requestTemperatures();
+  Serial.println("Got");
+  Serial.print("tempc0: ");
+  Serial.println(sensors.getTempCByIndex(0));  
+  Serial.print("tempc1: ");
+  Serial.println(sensors.getTempCByIndex(1));  
+  Serial.print("tempDeltaDiag(): ");
+  Serial.println(tempDeltaDiag());
+}
+
+
 void runFullDiag() {
    int localDelay = FULL_SYSTEM_RUNDOWN * 1000;
+
+  Serial.println("runFullDiag()");
   
   digitalWrite(HEAT_PUMP_PIN,HIGH);
+
+  Serial.println("runFullDiag() - StartDelay");
   delay(localDelay);
 
   if(tempDeltaDiag())   digitalWrite(HEAT_PUMP_PIN,HIGH);
@@ -88,6 +134,56 @@ void runFullDiag() {
 void runDiagChooser() {
   int state = digitalRead(HEAT_PUMP_PIN);
 
+  Serial.println("runDiagChooser()");
+  
+
   if(state && !tempDeltaDiag()) digitalWrite(HEAT_PUMP_PIN,LOW);
   if(!state)   runFullDiag();
+}
+
+void runHeatBreaker() {
+  int state = digitalRead(HEAT_PUMP_PIN);
+
+  if(state && !tempDeltaDiag()) digitalWrite(HEAT_PUMP_PIN,LOW);
+ 
+}
+
+void sendTemps() {
+  unsigned long currentMillis = millis();
+
+  if ( currentMillis - previousMillis > watchdog ) {
+    previousMillis = currentMillis;
+    WiFiClient client;
+  
+    if (!client.connect(host, port)) {
+      Serial.println("connection failed");
+      return;
+    }
+
+    String url = "/canaa/pool_be.php?millis=";
+    url += String(millis());
+    url += "&tempc0=";
+    url += sensors.getTempCByIndex(0);
+    url += "&tempc1=";
+    url += sensors.getTempCByIndex(1);
+    
+    // Envoi la requete au serveur - This will send the request to the server
+    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+    unsigned long timeout = millis();
+    while (client.available() == 0) {
+      if (millis() - timeout > 10000) {
+        Serial.println(">>> Client Timeout !");
+        client.stop();
+        return;
+      }
+    }
+  
+    // Read all the lines of the reply from server and print them to Serial
+    while(client.available()){
+      String line = client.readStringUntil('\r');
+      Serial.print(line);
+    }
+  }
 }
