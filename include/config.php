026e64bb54eda23d154944564df58eea0453f2e8
@yurisa2 MT5 <yuri@sa2.com.br>
2019-01-04 17:15:09 -0200

    Menu COnfig

--- include/config.php
+++ include/config.php
@@ -3,7 +3,8 @@
 $menu_items = array(
   "Status" => "index.php",
   "Temperaturas" => "temp_hist.php",
-  "Eventos" => "events.php"
+  "Eventos" => "events.php",
+  "Agendamento Bomba Principal" => "main_pump_time_picker.php"
 );
 
 $menu_title = "Canaa";
